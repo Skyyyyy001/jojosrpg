cb42b9d425b2a6557b6dc3475aa70e210bdbcff3
Skyyyyy001 <115953144+Skyyyyy001@users.noreply.github.com>
2022-10-16 19:27:39 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1665958088,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fotoperfil-sheet0.png",
+		"images/fotoperfil-sheet1.png",
+		"images/backgrounds-sheet0.png",
+		"images/coisas-sheet0.png",
+		"images/players-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
